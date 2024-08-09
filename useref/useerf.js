bc3c839a687a1497cbb005918f753ead55e4a08e
unknown <msaiteja203@gmail.com>
2024-08-09 16:51:44 +0530

    updated the useref

--- useref/useerf.js
+++ useref/useerf.js
@@ -1,7 +1,17 @@
+import { useRef, useState } from "react";
+
 function Usereff(){
+    const [count, setCount] = useState(0);
+
+    let aa = useRef();
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    }
     return(
         <div>
             <h1>USEREF REACT JS</h1>
+            <button ref={aa} onClick={handleIncrement}>Count Increment {count}</button>
         </div>
     )
 }
